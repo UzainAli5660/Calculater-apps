4d2ccb7f98f0471c8565c4ccec554d0fef93c462
UzainAli5660 <162098638+UzainAli5660@users.noreply.github.com>
2024-04-23 03:24:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+function Value(num) {
+    var inputField = document.getElementById("inputField")
+
+    inputField.value += num
+}
+
+function result() {
+    var inputField = document.getElementById("inputField")
+    inputField.value = eval(inputField.value)
+}
+
+
+function allClearValue() {
+    var inputField = document.getElementById("inputField")
+    inputField.value = ""
+}
+
+
+function singleClearValue() {
+    var inputField = document.getElementById("inputField")
+    inputField.value = inputField.value.slice(0, -1)
+}
